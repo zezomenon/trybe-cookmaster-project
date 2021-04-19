cc1b87643973c5458078e303ffc0e672b8db7ea9
Cristiano CUnha <contato@cristianoprogramador.com>
2021-04-19 16:41:53 -0300

    Adiciona listen da API

--- index.js
+++ index.js
@@ -2,7 +2,11 @@ const express = require('express');
 
 const app = express();
 
+const PORT = 3000;
+
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (request, response) => {
   response.send();
 });
+
+app.listen(PORT, () => { console.log('API rodando na porta 3000'); });
