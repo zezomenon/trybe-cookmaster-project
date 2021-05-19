6359b05eae93af73ef3ac69452ff9462d8999582
Israel Menon <zezomenon@gmail.com>
2021-05-19 11:12:53 -0300

    Refatorando cadastro usuarios - role = user

--- src/controllers/registerUserController.js
+++ src/controllers/registerUserController.js
@@ -4,9 +4,9 @@ const { registerUserService } = require('../services');
 const registerUserController = async (req, res, _next) => {
   try {
     const { name, email, password } = req.body;
-    const result = await registerUserService(name, email, password);
-    console.log(result);
-    await res.status(StatusCodes.CREATED).json(result);
+    const user = await registerUserService(name, email, password);
+    // console.log(result);
+    await res.status(StatusCodes.CREATED).json({ user });
   } catch (error) {
     const { status, message } = error;
     await res.status(status).json({
--- src/services/registerUserService.js
+++ src/services/registerUserService.js
@@ -3,7 +3,8 @@
 const { createUser } = require('../models/userModels');
 
 const registerUserService = async (name, email, password) => {
-  const result = await createUser(name, email, password);
+  const role = 'user';
+  const result = await createUser(name, email, password, role);
   // console.log(result);
   return result;
 };
